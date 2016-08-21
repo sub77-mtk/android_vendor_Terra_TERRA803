bac371b601a749e14da5d9a0afb93806624bce8b
sub77 <sub77@ymail.com>
2016-08-21 09:54:14 +0000

    HM2014011 -> TERRA803

---
+++ TERRA803-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Terra/TERRA803/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Terra/TERRA803/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Terra/TERRA803/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Terra/TERRA803/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Terra/TERRA803/xbin,system/xbin)
+
