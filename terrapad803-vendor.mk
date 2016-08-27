47b78c788d0966de68a8aff4933ca84d7325d050
sub77 <sub77@ymail.com>
2016-08-27 22:03:17 +0200

    Update Firmware

---
+++ terrapad803-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/terra/terrapad803/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/terra/terrapad803/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/terra/terrapad803/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/terra/terrapad803/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/terra/terrapad803/xbin,system/xbin)
